a9d47d12cc1a50536a4c535008181540305706f5
Data-drone <bpl.law@gmail.com>
2018-10-15 09:18:32 +0800

    finished three stack in one list python

--- coding_int/chapter_3/3_1_three_in_one_stack.py
+++ coding_int/chapter_3/3_1_three_in_one_stack.py
@@ -7,6 +7,11 @@ class fixed_size_array:
         self.values = [None for x in range(0, num_stacks*stack_size)]
         self.stackSize = stack_size
 
+    def __str__(self):
+        values = ' '.join([str(x) for x in self.values])
+        sizes = ' '.join([str(x) for x in self.sizes])
+        return 'values: {0}, sizes:{1}'.format(values, sizes)
+
         
     def push(self, value, stacknum):
         # add a value if the stack isn't full
@@ -23,18 +28,19 @@ class fixed_size_array:
     def pop(self, stacknum):
         # remove the top value and return it 
         if not self.isEmpty(stacknum):
-            index = stacknum*self.stackSize + self.sizes[stacknum]
+            index = stacknum*self.stackSize + self.sizes[stacknum]-1
             ret_value = self.values[index]
             self.values[index] = None
             self.sizes[stacknum] -= 1
-            return self.values[index]
+            return ret_value
         else:
             return None
     
     def peek(self, stacknum):
         # get top value in place without removing it
         if not self.isEmpty(stacknum):
-            return self.values[stacknum*self.stackSize + self.sizes[stacknum]+1]
+            #print('retrieving index: {0}'.format(stacknum*self.stackSize + self.sizes[stacknum]-1))
+            return self.values[stacknum*self.stackSize + self.sizes[stacknum]-1]
         else:
             return None
 
@@ -54,7 +60,7 @@ class fixed_size_array:
 if __name__ == "__main__":
 
 
-    test_stack = fixed_size_array(stack_size = 2)
+    test_stack = fixed_size_array(stack_size = 3)
 
 
     for i in range(0,3):
@@ -63,14 +69,26 @@ if __name__ == "__main__":
     for i in range(0,3):
         test_stack.push(value=3, stacknum=i)
         test_stack.push(value=2, stacknum=i)
+        test_stack.push(value=4, stacknum=i)
     
+    # is empty test
     for i in range(0,3):
         assert test_stack.isEmpty(i) == False, "isEmpty isn't working"
         assert test_stack.isFull(i) == True, "isFull isn't working"
 
+    # peek test
     for i in range(0,3):
-        print("value is: {0}".format(test_stack.peek(stacknum = i)))
-        assert test_stack.peek(stacknum = i) == 2, "peek is pulling wrong value"
+        #print("value is: {0}".format(test_stack.peek(stacknum = i)))
+        assert test_stack.peek(stacknum = i) == 4, "peek is pulling wrong value"
+
+    # pop test    
+    assert test_stack.pop(1) == 4, "pop not returning right value"
+    assert test_stack.peek(1) == 2, "pop is not removing value properly"
+
+    for i in range(0,3):
+        print("top value is: {0}".format(test_stack.peek(stacknum = i)))
+    
+
 
 
 
