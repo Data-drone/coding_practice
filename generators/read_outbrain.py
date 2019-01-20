c3da10fabf044d76d5ac7c1313586aa740f3761c
Data-drone <bpl.law@gmail.com>
2019-01-21 00:21:50 +0800

    changed the generator to get a list of distinct docus

--- generators/read_outbrain.py
+++ generators/read_outbrain.py
@@ -10,23 +10,20 @@ page_view_sample = 'page_views_sample.csv' # page views sample
 def field_map(dictseq, name, func):
     for d in dictseq:
         d[name] = func(d[name])
-        yield d
+        yield d        
 
 if __name__ == '__main__':
-
-    # page view columns
-    colnames = ('uuid', 'document_id', 'timestamp', 'platform', 
-                'geo_location', 'traffic_source')
 
     page_view_samp = os.path.join(data_folder, page_view_sample)
 
     with open(page_view_samp, 'r') as pageviews:
         headerline = next(pageviews).split(',')
         tuples = (dict(zip(headerline, line.split(','))) for line in pageviews)
-        tuples = field_map(tuples, "document_id", int)
-        lowid = (i for i in tuples if i['document_id'] < 1000)
+        docs = {r['document_id'] for r in tuples}
+        #tuples = field_map(tuples, "document_id", int)
+        #lowid = (i for i in tuples if i['document_id'] < 1000)
 
-        for r in lowid:
-            print(r['timestamp'])
+        #for r in lowid:
+        #    print(r['timestamp'])
     
-    #print(headerline)
+    print(docs)
